efc03926111ed42a677fd0f2def72657c2ae8a22
Johan Öbrink <johan.obrink@gmail.com>
2020-12-05 20:13:05 +0100

    Fixed insta feed

--- components/instagram/Feed.jsx
+++ components/instagram/Feed.jsx
@@ -1,3 +1,4 @@
+import { Col, Container, Image, Row } from 'react-bootstrap'
 import { Post } from './Post'
 import { useFeed } from './service'
 
@@ -6,16 +7,36 @@ export default function Feed ({ username }) {
 
   return (
     <>
-      { state === 'loading' && <div>Loading</div> }
-      { state === 'loaded' && <div>
-        <h2>{data.user.username}</h2>
-        <div><img src={data.user.pic} /></div>
-        <p>{data.user.biography}</p>
-        <div className="row">
-          {data.timeline.map((post) => (
-            <Post key={post.id} {...post} />
-          ))}
-        </div>
-    </div>}</>
+      <style jsx>{`
+        h6 {
+          margin-top: 10px;
+        }
+        .text {
+          font-size: 0.8em;
+          height: 75px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      `}</style>
+      <Container fluid className="instaFeed">
+        <Row>
+        { state === 'loading' && <Col>Loading</Col> }
+        { state === 'loaded' &&
+          <>
+            <Col>
+              <Image src={data.user.pic} fluid roundedCircle />
+              <h6>@{data.user.username}</h6>
+              <p className="text">{data.user.biography}</p>
+            </Col>
+            {data.timeline.slice(0, 3).map((post) => (
+              <Col>
+                <Post key={post.id} {...post} />
+              </Col>
+            ))}
+          </>
+        }
+        </Row>
+      </Container>
+    </>
   )
 }
