86f2ad30e4fb687809b0373750446b788e480dcd
Johan Öbrink <johan.obrink@gmail.com>
2020-12-06 11:19:13 +0100

    Only level 2 in main menu (#3)
    * Only level 2 in main menu
    
    fixes #2

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -7,7 +7,9 @@ export default function Menu ({menu}) {
   return (
     <header className="text-center px-5 py-4">
       <div>
-        <h3 className="float-md-left mb-0">Karlbergs-Bro</h3>
+        <h3 className="float-md-left mb-0 nav-brand">
+          <a className="nav-link" href="/">Karlbergs-Bro</a>
+        </h3>
         <Nav
           className="nav-masthead justify-content-center float-md-right"
           activeKey={path}>
