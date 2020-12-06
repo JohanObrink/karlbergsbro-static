a9a4abbfd51621054d4cedff247de14be2dad9a3
Johan Öbrink <johan.obrink@gmail.com>
2020-12-06 15:34:03 +0100

    Styling

--- components/Markdown.jsx
+++ components/Markdown.jsx
@@ -3,6 +3,6 @@ import gfm from 'remark-gfm'
 
 export default function Markdown ({children}) {
   return (
-    <ReactMarkdown plugins={[gfm]} className="text-left" children={children} />
+    <ReactMarkdown plugins={[gfm]} className="text-left markdown" children={children} />
   )
 }
