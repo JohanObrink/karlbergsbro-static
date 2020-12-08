feee90ee752a9e7a4632d4cb846603945237f819
Johan Öbrink <johan.obrink@gmail.com>
2020-12-08 17:07:56 +0100

    Flyttade insta-feed till sidfot
    fixes #21

--- components/StartPage.jsx
+++ components/StartPage.jsx
@@ -1,13 +1,11 @@
 import { Container } from "react-bootstrap"
-import Feed from "./instagram/Feed";
-import Markdown from "./Markdown";
+import Markdown from "./Markdown"
 import Masthead from "./Masthead"
 
 export default function StartPage ({
   content,
   image,
-  headline,
-  instagram
+  headline
 }) {
   return (
     <>
@@ -15,11 +13,8 @@ export default function StartPage ({
         <h1>{headline}</h1>
       </Masthead>
       <Container>
-      <Markdown>{content}</Markdown>
+        <Markdown>{content}</Markdown>
       </Container>
-      {instagram &&
-        <Feed username={instagram} />
-      }
     </>
   )
 }
