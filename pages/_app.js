b3f6f18a75681a318faba2d938dad95f8ffe1cf9
Johan Öbrink <johan.obrink@gmail.com>
2021-01-19 21:42:38 +0100

    fix: 🐛 Tagmanager (#42)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import '../styles/app.scss'
-import Router from 'next/router'
-import withGa from 'next-ga'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { pageview } from '../services/gtag'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
-export default withGa('G-N01WEE729Q', Router)(MyApp)
+export default MyApp
