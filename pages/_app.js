80b58270f59167367c259022c2aea81820b3f4d5
Johan Öbrink <johan.obrink@gmail.com>
2021-01-19 21:11:51 +0100

    feat: 🎸 Google Analytics (#41)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/app.scss'
+import Router from 'next/router'
+import withGa from 'next-ga'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withGa('G-N01WEE729Q', Router)(MyApp)
